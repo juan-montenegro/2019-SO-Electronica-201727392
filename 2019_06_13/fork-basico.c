ca54445b4e2bc8e5d22dc38079f9c11a193ee79c
invitado <invitado@s5pc7.eisc.univalle.edu.co>
2019-06-13 11:45:33 -0500

    fork-basico.c

--- 2019_06_13/fork-basico.c
+++ 2019_06_13/fork-basico.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 int main(int argc, char** argv){
 	pid_t pid;
 
+	printf("Hola");	
 	pid = fork();
-	printf("Hola");
+	printf("Hola a todos");
+
 	if (pid != 0){
-		printf("Hola a todos");
 		printf("soy el padre mi id es %d"
 		" y el de mi hijo es %d\n",getpid(),pid);
 		}
 	else{
-		printf("Hola a todos");
 		printf("soy el hijo con id %d"
 		" y mi variable pid es %d\n",getpid(),pid);
 	}
