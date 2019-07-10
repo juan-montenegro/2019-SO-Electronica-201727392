0403972693d3baf2465331231ea4805dcf03b287
Juan Montenegro <montenegro.juan@correounivalle.edu.co>
2019-07-10 15:17:42 -0500

    myshell

--- Proyecto-Shell/myshell.c
+++ Proyecto-Shell/myshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -24,17 +25,86 @@ int initScreen(void){
 	sleep(1);
 }
 
-int currentDir(void) { 
+int newLine(void) { 
     char cwd[256]; 
     getcwd(cwd, sizeof(cwd)); 
+	clear();
     printf("\nShell= %s", cwd); 
-} 
+}
 
-int cmd
+int newLine(void) { 
+    char cwd[256]; 
+    getcwd(cwd, sizeof(cwd)); 
+	clear();
+    printf("\nShell= %s", cwd); 
+}
+
+int currentDir (char** cDir){
+	DIR *dp;
+	struct dirent *ep;
+	
+	dp = opendir (cDir);
+	if (dp != NULL){
+		while (ep = readdir (dp))
+		puts (ep->d_name);
+    	(void) closedir (dp);
+    }
+	else
+    	printf("Directorio no válido");
+  		return 0;
+}
+
+int commandAdmin(char** parsed){
+	int commandNumber = 8;
+	int i;
+	int argSearch = 0;
+	char* commandList[commandNumber];
+	commandList[0] = "cd";
+	commandList[1] = "clr";
+	commandList[2] = "dir";
+	commandList[3] = "environ";
+	commandList[4] = "echo";
+	commandList[5] = "help";
+	commandList[6] = "pause";
+	commandList[7] = "quit";
+
+	for (i = 0; i < commandNumber; i++){
+		if (strcmp(parsed[0]) = commandList[i] == 0){
+			argSearch = i + 1;
+			break;
+		}	
+	}
+	switch (argSearch){
+		case 1:
+			chdir(parsed[1]);
+			return 1;
+		case 2:
+			newLine();
+			return 1;
+		case 3:
+			currentDir(parsed[1]);
+			return 1;
+		case 4:
+
+		case 5:
+
+		case 6:
+
+		case 7:
+
+		case 8:
+			printf("Saliendo del terminal\n");
+			sleep(1);
+			exit(0);
+		default:
+			break;
+	}
+	return 0;
+}
 
 int main(int argc, char** argv) {
 	initScreen();
 	while (1){
-
+		break;
 	}
 }
