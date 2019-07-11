e1821650cce9e15f85272c8ba6320341d2d70dc7
Invitado <invitado@s5pc7.eisc.univalle.edu.co>
2019-07-11 12:51:12 -0500

    clase

---
+++ 2019_07_11/memory-user.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int arg, char** argv){
+	if (arg != 2){
+		printf("Porfavor digitar un numero.\n");
+		exit(-1)
+	}
+	int memorysize = atoi(argv[1]));
+	memorysize = 1024*1024;
+	int *arreglo = (int*) malloc(sizeof(int) * memorysize;
+	if (arreglo == NULL) {
+		printf("Error en la asignacion de memoria\n");
+		exit(-1)
+	}
+	int i = 0;
+	for  (; i < memorysize; i++){
+		arreglo[i] = i;
+	}
+	sleep(10);
+	free(arreglo);
+	return 0;
+}
